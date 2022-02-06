228c8bfab52c7c2efefb98309e66c6dcf035fc6e
Mr.GeschVed <86187817+chilledboyzrblx@users.noreply.github.com>
2022-02-06 17:06:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ function startClassification(){
 function modelReady(){
     classifier.classify(gotResults);
 }
+
+var dog = 0;
+var cat = 0;
+var cow = 0;
+var bird = 0;
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random()*255)+1;
+        random_number_g = Math.floor(Math.random()*255)+1;
+        random_number_b = Math.floor(Math.random()*255)+1;
+
+        document.getElementById("result_sound").innerHTML = 'Detected sound of - '+results[0].label;
+        document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+' Detected Cat - '+cat+' Detected Cow - '+cow+' Detected Bird - '+bird;
+        document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+        document.getElementById("result_sound").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+        img = document.getElementById('animal_image');
+
+        if(results[0].label == "Meowing"){
+            img.src = 'cat.png';
+            cat = cat + 1;
+        } else if(results[0].label == "Barking"){
+            img.src = 'dog.png';
+            dog = dog + 1;
+        } else if(results[0].label == "Chirping"){
+            img.src='bird.png';
+            bird = bird + 1;
+        } else if(results[0].label == "Mooing"){
+            img.src='cow.png';
+            cow = cow + 1;
+        } else {
+            img.src = 'ear.png';
+        }
+    }
+}
